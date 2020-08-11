8c21f88c46996215673cca1a79998d4182a92564
Blesswin Samuel <blesswinsamuel@gmail.com>
2020-08-11 10:14:16 +0530

    update maintest

--- maintest/rpi.go
+++ maintest/rpi.go
@@ -1,55 +1,87 @@
 package main
 
 import (
-	"fmt"
+	"time"
 
-	// "github.com/blesswinsamuel/rpi_exporter/d2r2/dht"
-
-	"github.com/blesswinsamuel/rpi_exporter/dht"
 	"github.com/prometheus/common/log"
+	"periph.io/x/periph/conn/gpio"
 	"periph.io/x/periph/host"
 	"periph.io/x/periph/host/rpi"
 )
 
+func light(pin gpio.PinIO, ch <-chan bool) {
+	for {
+		log.Debugf("Light waiting")
+		v := gpio.High
+		if <-ch {
+			v = gpio.High
+		} else {
+			v = gpio.Low
+		}
+		log.Debugf("Setting light to %s", v)
+		if err := pin.Out(v); err != nil {
+			log.Error(err)
+		}
+	}
+}
+
+func buzzer(pin gpio.PinIO, ch <-chan bool) {
+	for {
+		v := gpio.High
+		log.Debugf("Buzzer waiting")
+		if <-ch {
+			v = gpio.High
+		} else {
+			v = gpio.Low
+		}
+		log.Debugf("Setting buzzer to %s", v)
+		if err := pin.Out(v); err != nil {
+			log.Error(err)
+		}
+	}
+}
+
+// func ldr(ch chan<- bool) {
+// 	for l := gpio.Low; ; l = !l {
+// 		if err := rpi.P1_13.ADC(); err != nil {
+// 			log.Fatal(err)
+// 		}
+// 	}
+// }
+
 func main() {
-	// temperature, humidity, retried, err :=
-	// 	dht.ReadDHTxxWithRetry(dht.DHT11, rpi.P1_7, false, 10)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
 	log.Base().SetLevel("debug")
 	if _, err := host.Init(); err != nil {
 		log.Fatal(err)
 	}
-	d, err := dht.NewDHT(rpi.P1_7, dht.Celsius, "dht11")
-	if err != nil {
-		log.Fatal(err)
-	}
-	humidity, temperature, retries, err := d.ReadRetry(11)
-	if err != nil {
-		fmt.Println("Read error:", err)
-		return
-	}
+	// analog.PinADC{
 
-	fmt.Printf("humidity: %v\n", humidity)
-	fmt.Printf("temperature: %v\n", temperature)
-	fmt.Printf("retries: %v\n", retries)
-
-	// temperature, humidity, retried, err :=
-	// 	dht.ReadDHTxxWithRetry(dht.DHT11, rpi.P1_7, false, 10)
+	// }
+	// d, err := dht.NewDHT(rpi.P1_7, dht.Celsius, "dht11")
 	// if err != nil {
 	// 	log.Fatal(err)
 	// }
-	// // Print temperature and humidity
-	// fmt.Printf("Temperature = %v*C, Humidity = %v%% (retried %d times)\n",
-	// 	temperature, humidity, retried)
+	// humidity, temperature, retries, err := d.ReadRetry(11)
+	// if err != nil {
+	// 	fmt.Println("Read error:", err)
+	// 	return
+	// }
+
+	// fmt.Printf("humidity: %v\n", humidity)
+	// fmt.Printf("temperature: %v\n", temperature)
+	// fmt.Printf("retries: %v\n", retries)
 
 	// t := time.NewTicker(500 * time.Millisecond)
-	// for l := gpio.Low; ; l = !l {
-	// 	// Lookup a pin by its location on the board:
-	// 	if err := rpi.P1_7.Out(l); err != nil {
-	// 		log.Fatal(err)
-	// 	}
-	// 	<-t.C
-	// }
+	ledCh := make(chan bool)
+	buzzerCh := make(chan bool)
+
+	go light(rpi.P1_11, ledCh)
+	go buzzer(rpi.P1_13, buzzerCh)
+
+	ledCh <- true
+	buzzerCh <- true
+	time.Sleep(time.Second)
+	ledCh <- false
+	buzzerCh <- false
+	time.Sleep(time.Millisecond)
 }
