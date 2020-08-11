171a3f26e622731f29520b64f3425bea13eb3860
Blesswin Samuel <blesswinsamuel@gmail.com>
2020-08-11 18:32:56 +0530

    update install firmware script

--- install_firmware.sh
+++ install_firmware.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-https://downloads.arduino.cc/arduino-cli/arduino-cli_latest_Linux_ARMv7.tar.gz
-
-arduino-cli compile --fqbn arduino:avr:uno --upload --port /dev/cu.usbmodem1421301 ./firmware
+wget -P /tmp/firmware https://raw.githubusercontent.com/blesswinsamuel/arduino_exporter/master/firmware/firmware.ino
+arduino-cli compile --fqbn arduino:avr:uno --upload --port /dev/ttyACM0 /tmp/firmware
